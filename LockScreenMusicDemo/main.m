859d43c4dace5a00f27dfe78da08f29778954de9
shuzhenguo <shuzhenguovip@163.com>
2017-03-24 08:59:27 +0800

    Initial Commit

---
+++ LockScreenMusicDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LockScreenMusicDemo
+//
+//  Created by shuzhenguo on 2017/3/24.
+//  Copyright © 2017年 shuzhenguo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
